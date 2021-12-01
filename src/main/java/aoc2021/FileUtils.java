4392153330b821413316a67945c236b38ae80fb7
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-01 14:08:13 -0500

    Improvements to FileUtils

--- src/main/java/aoc2021/FileUtils.java
+++ src/main/java/aoc2021/FileUtils.java
@@ -1,14 +1,11 @@
 package aoc2021;
 
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -28,11 +25,12 @@ public final class FileUtils {
     /**
      * Read a file, available on the classpath, into a {@link List} of strings.
      * 
-     * @param fileName The name of a file which can be found on the classpath.
-     * @return A {@link List} of strings, one for each line in the file. Returns an
-     *     empty list if there were any errors opening the file.
+     * @param fileName
+     *            The name of a file which can be found on the classpath.
+     * @return A {@link List} of strings, one for each line in the file. Returns
+     *         an empty list if there were any errors opening the file.
      */
-    public static List<String> readFile(String fileName) {
+    public static List<String> readFile(final String fileName) {
         try {
             return Files.readAllLines(Paths.get(ClassLoader.getSystemResource(fileName).toURI()));
         } catch (IOException | URISyntaxException e) {
@@ -42,14 +40,16 @@ public final class FileUtils {
     }
 
     /**
-     * Read a file, available on the classpath, into a {@link Stream} of strings.
-     * The stream can only be read from once.
+     * Read a file, available on the classpath, into a {@link Stream} of
+     * strings. The stream can only be read from once.
      * 
-     * @param fileName The name of a file which can be found on the classpath.
-     * @return A {@link Stream} of strings, one for each line in the file. Returns
-     *     an empty stream if there were any errors opening the file.
+     * @param fileName
+     *            The name of a file which can be found on the classpath.
+     * @return A {@link Stream} of strings, one for each line in the file.
+     *         Returns an empty stream if there were any errors opening the
+     *         file.
      */
-    public static Stream<String> readFileToStream(String fileName) {
+    public static Stream<String> readFileToStream(final String fileName) {
         try {
             return Files.lines(Paths.get(ClassLoader.getSystemResource(fileName).toURI()));
         } catch (IOException | URISyntaxException e) {
@@ -58,29 +58,4 @@ public final class FileUtils {
         }
     }
 
-    /**
-     * Clean the input: Replace newlines with spaces, then split along double
-     * spaces.
-     * 
-     * @param lines The stream of lines from the input file. The data may be
-     *     separated by newlines, records are separated by two newlines.
-     * @return A stream of complete records, separated internally by spaces.
-     */
-    public static Stream<String> cleanInput(Stream<String> lines) {
-        return lines.collect(Collectors.collectingAndThen(Collectors.joining(" "),
-                                                          s -> Stream.of(s.split("  "))));
-    }
-
-    /**
-     * Clean the input: Replace newlines with spaces, then split along double
-     * spaces.
-     * 
-     * @param lines The stream of lines from the input file. The data may be
-     *     separated by newlines, records are separated by two newlines.
-     * @return A stream of complete records, separated internally by spaces.
-     */
-    public static List<String> cleanInput(List<String> lines) {
-        return lines.stream().collect(Collectors.collectingAndThen(Collectors.joining(" "),
-                                                                   s -> Arrays.asList(s.split("  "))));
-    }
 }
