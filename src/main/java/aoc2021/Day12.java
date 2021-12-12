5addfcd7549d7ab4e5bc07a4f6f0eb300bebbe89
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-12 01:06:01 -0500

    Implement Day 12 - part 2

--- src/main/java/aoc2021/Day12.java
+++ src/main/java/aoc2021/Day12.java
@@ -57,11 +57,15 @@ public class Day12 {
 
         log.setLevel(Level.DEBUG);
 
-        log.info("{}", part2(testLines));
+        i = 1;
+        for (String testLine : testLines) {
+            log.info("There are {} paths through test example {} for part 2.",
+                     part2(Arrays.asList(testLine.split(" "))), i++);
+        }
 
         log.setLevel(Level.INFO);
 
-        log.info("{}", part2(lines));
+        log.info("There are {} paths through the real data for part 2.", part2(lines));
     }
 
     /**
@@ -94,11 +98,46 @@ public class Day12 {
         return traverseCaves(caves.get("start"), new Stack<>());
     }
 
+    /**
+     * How many paths through this cave system are there that visit a single small
+     * caves at most twice?
+     * 
+     * @param lines The lines representing the connections between the caves.
+     * @return The number of the paths through the caves which only visit a small
+     *     caves at most twice.
+     */
     private static int part2(final List<String> lines) {
 
-        return -1;
+        // Map out all the caves
+        Map<String, Cave> caves = new HashMap<>();
+        lines.stream()
+             .flatMap(l -> Stream.of(l.split("-")))
+             .forEach(name -> caves.computeIfAbsent(name, Cave::new));
+
+        // Create the links
+        lines.forEach(l -> {
+            String[] names = l.split("-");
+            Cave cave1 = caves.get(names[0]);
+            Cave cave2 = caves.get(names[1]);
+            // Don't link back to "start" and "end."
+            if (!("start".equals(cave2.name) || "end".equals(cave1.name)))
+                cave1.connectedCaves.add(cave2);
+            if (!("start".equals(cave1.name) || "end".equals(cave2.name)))
+                cave2.connectedCaves.add(cave1);
+        });
+
+        log.debug("Caves: \n{}", caves.values());
+
+        return traverseCaves2(caves.get("start"), new Stack<>());
     }
 
+    /**
+     * Traverse the caves, visiting small caves at most once.
+     * 
+     * @param start The cave to start in.
+     * @param visited The path taken to get to the current cave.
+     * @return The number of routes to the end from the current cave.
+     */
     private static int traverseCaves(Cave start, Stack<Cave> visited) {
         log.trace(start.name);
 
@@ -112,7 +151,7 @@ public class Day12 {
         int routes = 0;
         // Visit all connected caves
         for (Cave next : start.connectedCaves) {
-            if (visited.contains(next) && next.small)
+            if (next.small && visited.contains(next))
                 continue;
             routes += traverseCaves(next, visited);
         }
@@ -121,6 +160,46 @@ public class Day12 {
         return routes;
     }
 
+    /**
+     * Traverse the caves, visiting a single small cave at most twice.
+     * 
+     * @param start The cave to start in.
+     * @param visited The path taken to get to the current cave.
+     * @return The number of routes to the end from the current cave.
+     */
+    private static int traverseCaves2(Cave start, Stack<Cave> visited) {
+        log.trace(start.name);
+
+        if ("end".equals(start.name)) {
+            log.debug("{},end", visited.stream().map(c -> c.name).collect(Collectors.joining(",")));
+            return 1;
+        }
+
+        visited.push(start);
+
+        int routes = 0;
+        // Visit all connected caves
+        for (Cave next : start.connectedCaves) {
+            // If this is a small cave which we've already visited, and there are already
+            // two identical small caves in the list of visited caves; skip it
+            if (next.small && visited.contains(next)
+                && visited.stream()
+                          .filter(c -> c.small)
+                          .collect(Collectors.groupingBy(c -> c.name, Collectors.counting()))
+                          .values()
+                          .contains(2L))
+                continue;
+            
+            routes += traverseCaves2(next, visited);
+        }
+
+        visited.pop();
+        return routes;
+    }
+
+    /**
+     * Representation of a cave connected to other caves.
+     */
     private static class Cave {
         String name;
         boolean small;
