f77f84a6599f3ff8ae795c5e83bfe0bd7dcc735d
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-14 12:47:34 -0500

    Implement Day 14 - part 2 as a graph
    It's still slow, I think I can improve. Also, I don't know if this gives
    me a solution.

--- src/main/java/aoc2021/Day14.java
+++ src/main/java/aoc2021/Day14.java
@@ -1,5 +1,6 @@
 package aoc2021;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,7 +38,8 @@ public class Day14 {
                                                  .map(l -> l.split(" -> "))
                                                  .collect(Collectors.toMap(r -> r[0], r -> r[1]));
 
-        log.info("{}", part1(testRules, TEST_POLYMER_TEMPLATE));
+        log.info("After 10 iterations, the difference between the maximum and minimum occurrences of the characters in the test data is: {}.",
+                 part1(testRules, TEST_POLYMER_TEMPLATE));
 
         log.setLevel(Level.INFO);
 
@@ -46,17 +48,19 @@ public class Day14 {
                                              .map(l -> l.split(" -> "))
                                              .collect(Collectors.toMap(r -> r[0], r -> r[1]));
 
-        log.info("{}", part1(rules, POLYMER_TEMPLATE));
+        log.info("After 10 iterations, the difference between the maximum and minimum occurrences of the characters in the real data is: {}.",
+                 part1(rules, POLYMER_TEMPLATE));
 
         // PART 2
 
         log.setLevel(Level.DEBUG);
 
-        //        log.info("{}", part2(testLines));
+        log.info("After 40 iterations, the difference between the maximum and minimum occurrences of the characters in the test data is: {}.",
+                 part2(testRules, TEST_POLYMER_TEMPLATE));
 
         log.setLevel(Level.INFO);
 
-        //        log.info("{}", part2(lines));
+        //        log.info("After 40 iterations, the difference between the maximum and minimum occurrences of the characters in the real data is: {}.", part2(lines));
     }
 
     /**
@@ -73,9 +77,7 @@ public class Day14 {
 
         log.debug(polymerTemplate);
 
-        int itterations = 10;
-
-        for (int n = 1; n <= itterations; n++) {
+        for (int n = 1; n <= 10; n++) {
             char[] chars = polymerTemplate.toCharArray();
             char[] newPolymer = new char[chars.length * 2 - 1];
             for (int i = 0; i < chars.length - 1; i++) {
@@ -99,9 +101,95 @@ public class Day14 {
         return max - min;
     }
 
-    private static int part2(final List<String> lines) {
+    /**
+     * Determine the number of times each character will appear in the final
+     * string after 40 iterations, and return the difference between the maximum
+     * and the minimum.
+     * 
+     * @param insertionRules
+     *            The mapping of character pairs to an insertion character.
+     * @param polymerTemplate
+     *            The initial string of characters to apply the insertion rules
+     *            to.
+     * @return The difference between the maximum and the minimum occurrences of
+     *         the individual characters.
+     */
+    private static long part2(final Map<String, String> insertionRules, String polymerTemplate) {
 
-        return -1;
+        // Try creating a graph of the pairs, and traversing it for 40 steps deep
+        Map<String, Node> nodeMap = insertionRules.keySet().stream().map(Node::new).collect(Collectors.toMap(n -> n.value, n -> n));
+        insertionRules.entrySet().forEach(e -> {
+            String key = e.getKey();
+            String common = e.getValue();
+            Node root = nodeMap.computeIfAbsent(key, Node::new);
+            root.common = common;
+            root.left = nodeMap.computeIfAbsent(key.charAt(0) + common, Node::new);
+            root.right = nodeMap.computeIfAbsent(common + key.charAt(1), Node::new);
+        });
+
+        log.debug("Node map:\n{}", nodeMap.values().stream().map(Node::toString).collect(Collectors.joining("\n")));
+
+        // For each starting pair, traverse the graph and count the instances of each character.
+        Map<Character, Long> counts = new HashMap<>();
+
+        // Start with the last character already in the map.
+        counts.put(polymerTemplate.charAt(polymerTemplate.length() - 1), 1L);
+
+        char[] chars = polymerTemplate.toCharArray();
+        List<Node> startingNodes = new ArrayList<>();
+        for (int i = 0; i < chars.length - 1; i++) {
+            startingNodes.add(nodeMap.get(new String(new char[] { chars[i], chars[i + 1] })));
+        }
+
+        log.debug("Starting nodes: {}", startingNodes.stream().map(n -> n.value).collect(Collectors.joining(", ")));
+
+        startingNodes.stream()
+                     .flatMap(n -> countChars(n, 39).entrySet().stream())
+                     .forEach(e -> counts.merge(e.getKey(), e.getValue(), Math::addExact));
+
+        log.debug("Counts:\n{}", counts);
+
+        long max = counts.entrySet().stream().mapToLong(Entry::getValue).max().getAsLong();
+        long min = counts.entrySet().stream().mapToLong(Entry::getValue).min().getAsLong();
+
+        return max - min;
     }
 
+    private static Map<Character, Long> countChars(Node fromNode, int depth) {
+
+        Map<Character, Long> counts = new HashMap<>();
+
+        if (depth > 0) {
+            countChars(fromNode.left, depth - 1).entrySet().forEach(e -> counts.merge(e.getKey(), e.getValue(), Math::addExact));
+            countChars(fromNode.right, depth - 1).entrySet().forEach(e -> counts.merge(e.getKey(), e.getValue(), Math::addExact));
+        } else if (depth == 0) {
+
+            char leftChar = fromNode.value.charAt(0);
+            char commonChar = fromNode.common.charAt(0);
+            if (leftChar == commonChar) {
+                counts.put(leftChar, 2L);
+            } else {
+                counts.put(leftChar, 1L);
+                counts.put(commonChar, 1L);
+            }
+        }
+
+        return counts;
+    }
+
+    private static class Node {
+        String value;
+        Node left;
+        Node right;
+        String common;
+
+        Node(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s -> %s | %s", this.value, left.value, right.value);
+        }
+    }
 }
