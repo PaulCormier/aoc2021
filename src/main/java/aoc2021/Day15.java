782d672c50fe4d36dde091eabb604c569cf09cbd
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-15 11:33:50 -0500

    Implement Day 15 - part 1 and partial 2
    It runs very quickly now, but I'm getting the wrong answer for part 2
    with the real data.
    
    Big question: Does the path only go right and down?
    
    Tried going up and left, but ended up with a stack overflow.

--- src/main/java/aoc2021/Day15.java
+++ src/main/java/aoc2021/Day15.java
@@ -1,10 +1,12 @@
 package aoc2021;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
@@ -52,45 +54,86 @@ public class Day15 {
 
         log.setLevel(Level.DEBUG);
 
-        log.info("{}", part2(testMap));
+        log.info("The length of the path with the lowest risk level in the bigger test data is: {}.", part2(testMap));
 
-        log.setLevel(Level.INFO);
+        log.setLevel(Level.DEBUG);
+        //        log.setLevel(Level.INFO);
 
-        log.info("{}", part2(map));
+        log.info("The length of the path with the lowest risk level in the bigger real data is: {}.", part2(map));
     }
 
     /**
      * Determine the lowest total risk level of a path from the top left to the
      * bottom right of the map.
      * 
-     * @param map The map of the risk levels.
+     * @param map
+     *            The map of the risk levels.
      * @return The lowest total risk level.
      */
     private static int part1(final int[][] map) {
 
         // The maximum score is the shortest path times maximum risk
-        int maxScore = (map.length + map[0].length - 2) * 9;
+        //        int maxScore = (map.length + map[0].length - 2) * 9;
 
-        return traverseFrom(0, 0, map, maxScore) - map[0][0];
+        return traverseFrom(0, 0, map, new int[map.length][map[0].length]) - map[0][0];
     }
 
-    private static int part2(final int[][] lines) {
+    /**
+     * Determine the lowest total risk level of a path from the top left to the
+     * bottom right of the map when tiled 5 times in each direction.
+     * 
+     * @param map
+     *            The map of the risk levels.
+     * @return The lowest total risk level.
+     */
+    private static int part2(final int[][] map) {
 
-        return -1;
+        log.debug("Old map:\n{}", printMap(map));
+
+        // Grow the map
+        int[][] bigMap = Stream.of(Arrays.copyOf(map, map.length * 5))
+                               .map(r -> Arrays.copyOf(ObjectUtils.defaultIfNull(r, new int[map[0].length]), map[0].length * 5))
+                               .collect(Collectors.toList())
+                               .toArray(new int[map.length * 5][map[0].length * 5]);
+
+        // Copy and increment the original map
+        int[] newValues = IntStream.rangeClosed(0, 34)
+                                   .map(n -> (n - 1) % 9 + 1)
+                                   .toArray();
+        log.debug("Number map: {}", newValues);
+
+        for (int gridRow = 0; gridRow < 5; gridRow++) {
+            for (int gridCol = 0; gridCol < 5; gridCol++) {
+                for (int row = 0; row < map.length; row++) {
+                    for (int col = 0; col < map[row].length; col++) {
+                        bigMap[gridRow * map.length + row][gridCol * map[row].length + col] = newValues[map[row][col] + gridRow + gridCol];
+                    }
+                }
+            }
+        }
+
+        log.debug("New map:\n{}", printMap(bigMap));
+
+        log.setLevel(Level.INFO);
+        return traverseFrom(0, 0, bigMap, new int[bigMap.length][bigMap[0].length]) - bigMap[0][0];
     }
 
     /**
      * Start from the given coordinates and try paths from the given coordinates
      * until the max score is reached, or the bottom right corner.
      * 
-     * @param x Starting horizontal coordinate
-     * @param y Starting vertical coordinate
-     * @param map The map of risk levels
-     * @param maxScore The maximum score a path can be
-     * @return The minimum score from the starting point to the end, or the maximum
-     *     score parameter if reached.
+     * @param x
+     *            Starting horizontal coordinate
+     * @param y
+     *            Starting vertical coordinate
+     * @param map
+     *            The map of risk levels
+     * @param maxScore
+     *            The maximum score a path can be
+     * @return The minimum score from the starting point to the end, or the
+     *         maximum score parameter if reached.
      */
-    private static int traverseFrom(int x, int y, int[][] map, int maxScore) {
+    private static int traverseFrom(int x, int y, int[][] map, int[][] maxScore) {
 
         int score = map[y][x];
 
@@ -101,17 +144,81 @@ public class Day15 {
         }
 
         // Try going right
-        int rightScore = maxScore;
-        if (x + 1 < map[0].length)
-            rightScore = traverseFrom(x + 1, y, map, maxScore - score);
+        int rightScore = Integer.MAX_VALUE;
+        if (x + 1 < map[y].length)
+            rightScore = maxScore[y][x + 1] == 0 ? traverseFrom(x + 1, y, map, maxScore) : maxScore[y][x + 1];
 
         // Try going down
-        int downScore = maxScore;
+        int downScore = Integer.MAX_VALUE;
         if (y + 1 < map.length)
-            downScore = traverseFrom(x, y + 1, map, maxScore - score);
+            downScore = maxScore[y + 1][x] == 0 ? traverseFrom(x, y + 1, map, maxScore) : maxScore[y + 1][x];
 
         score += Math.min(rightScore, downScore);
         log.debug("From ({},{}) the minimum score is: {}", x, y, score);
+
+        maxScore[y][x] = score;
+
+        return score;
+    }
+
+    /**
+     * RESULTS IN A STACK OVERFLOW!
+     * 
+     * Start from the given coordinates and try paths from the given coordinates
+     * until the max score is reached, or the bottom right corner.
+     * 
+     * @param x
+     *            Starting horizontal coordinate
+     * @param y
+     *            Starting vertical coordinate
+     * @param map
+     *            The map of risk levels
+     * @param maxScore
+     *            The maximum score a path can be
+     * @param visited
+     *            A stack of previously visited locations.
+     * @return The minimum score from the starting point to the end, or the
+     *         maximum score parameter if reached.
+     */
+    private static int traverseFrom(int x, int y, int[][] map, int[][] maxScore, Stack<int[]> visited) {
+
+        int[] currentLocation = new int[] { x, y };
+        //        if (visited.contains(currentLocation))
+        //            return Integer.MAX_VALUE;
+
+        int score = map[y][x];
+        visited.push(currentLocation);
+
+        // If we're already in the bottom right corner, that's it.
+        if (y == map.length - 1 && x == map[0].length - 1) {
+            //            log.debug("Bottom right corner score: {}", score);
+            return score;
+        }
+
+        // Try going right
+        int rightScore = Integer.MAX_VALUE;
+        if (x + 1 < map[y].length && !visited.contains(new int[] { x + 1, y }))
+            rightScore = maxScore[y][x + 1] == 0 ? traverseFrom(x + 1, y, map, maxScore, visited) : maxScore[y][x + 1];
+
+        // Try going down
+        int downScore = Integer.MAX_VALUE;
+        if (y + 1 < map.length && !visited.contains(new int[] { x, y + 1 }))
+            downScore = maxScore[y + 1][x] == 0 ? traverseFrom(x, y + 1, map, maxScore, visited) : maxScore[y + 1][x];
+
+        // What about up, or left?
+        int upScore = Integer.MAX_VALUE;
+        if (y - 1 > 0 && !visited.contains(new int[] { x, y - 1 }))
+            upScore = maxScore[y - 1][x] == 0 ? traverseFrom(x, y - 1, map, maxScore, visited) : maxScore[y - 1][x];
+
+        int leftScore = Integer.MAX_VALUE;
+        if (x - 1 > 0 && !visited.contains(new int[] { x - 1, y }))
+            leftScore = maxScore[y][x - 1] == 0 ? traverseFrom(x - 1, y, map, maxScore, visited) : maxScore[y][x - 1];
+
+        score += Math.min(Math.min(rightScore, downScore), Math.min(upScore, leftScore));
+        //        log.debug("From ({},{}) the minimum score is: {}", x, y, score);
+
+        maxScore[y][x] = score;
+        visited.pop();
 
         return score;
     }
