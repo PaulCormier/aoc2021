dd954e20f308106bd8706637f846e1d02703eb52
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-16 14:58:47 -0500

    Implement Day 16 - part 1

---
+++ src/main/java/aoc2021/Day16.java
@@ -0,0 +1,173 @@
+package aoc2021;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+
+/**
+ * https://adventofcode.com/2021/day/16
+ * 
+ * @author Paul Cormier
+ *
+ */
+public class Day16 {
+
+    private static final Logger log = ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(Day16.class);
+
+    private static final String INPUT_TXT = "Input-Day16.txt";
+
+    private static final String TEST_INPUT_TXT = "TestInput-Day16.txt";
+
+    public static void main(String[] args) {
+
+        log.setLevel(Level.DEBUG);
+
+        // Read the test file
+        List<String> testLines = FileUtils.readFile(TEST_INPUT_TXT);
+        log.trace(testLines.toString());
+
+        for (String line : testLines)
+            log.info("The version sum of {} is: {}", line, part1(line));
+
+        log.setLevel(Level.INFO);
+
+        // Read the real file
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        log.info("The version sum of the real data is: {}", part1(lines.get(0)));
+
+        // PART 2
+
+        log.setLevel(Level.DEBUG);
+
+        log.info("{}", part2(testLines));
+
+        log.setLevel(Level.INFO);
+
+        log.info("{}", part2(lines));
+    }
+
+    /**
+     * Parse the input line into packets and return the sum of the version
+     * numbers.
+     * 
+     * @param inputLine
+     *            The hexadecimal encoded command line.
+     * @return The sum of the version numbers in the command line.
+     */
+    private static int part1(final String inputLine) {
+
+        // Convert to binary
+        String bits = inputLine.chars()
+                               .mapToObj(ch -> Integer.toBinaryString(Character.getNumericValue(ch)))
+                               .map(s -> StringUtils.leftPad(s, 4, '0'))
+                               .collect(Collectors.joining());
+        log.debug("{} => {}", inputLine, bits);
+
+        Packet packet = new Packet(bits);
+        log.debug(packet.toString());
+
+        // Sum the version numbers
+        Queue<Packet> packetsToCheck = new LinkedList<>();
+        packetsToCheck.add(packet);
+        int versionSum = 0;
+        while (!packetsToCheck.isEmpty()) {
+            Packet currentPacket = packetsToCheck.poll();
+            versionSum += currentPacket.version;
+            packetsToCheck.addAll(currentPacket.subPackets);
+        }
+
+        return versionSum;
+    }
+
+    private static int part2(final List<String> lines) {
+
+        return -1;
+    }
+
+    private static class Packet {
+        int version;
+        int type;
+
+        long value;
+
+        List<Packet> subPackets = new ArrayList<>();
+
+        // The number of bits in the binary representation of this packet. 
+        int bitCount = 6;
+
+        Packet(String bits) {
+            this.version = Integer.parseInt(bits.substring(0, 3), 2);
+            this.type = Integer.parseInt(bits.substring(3, 6), 2);
+
+            // Handle data packets
+            if (this.type == 4) {
+                List<String> data = Pattern.compile(".{5}")
+                                           .matcher(bits.substring(6))
+                                           .results()
+                                           .map(m -> m.group())
+                                           .collect(Collectors.toList());
+
+                StringBuilder valueString = new StringBuilder();
+                for (String nibble : data) {
+                    this.bitCount += 5;
+
+                    valueString.append(nibble.substring(1));
+
+                    if (nibble.charAt(0) == '0')
+                        break;
+                }
+                this.value = Long.parseLong(valueString.toString(), 2);
+            }
+
+            // Operators
+            else {
+                bitCount++;
+                if (bits.charAt(6) == '0') {
+                    int subPacketBits = Integer.parseInt(bits.substring(bitCount, bitCount + 15), 2);
+                    this.bitCount += 15;
+                    // Parse them into packets
+                    int offset = this.bitCount;
+                    this.bitCount += subPacketBits;
+                    while (subPacketBits > 0) {
+                        Packet subPacket = new Packet(bits.substring(offset, offset + subPacketBits));
+                        this.subPackets.add(subPacket);
+                        subPacketBits -= subPacket.bitCount;
+                        offset += subPacket.bitCount;
+                    }
+                } else {
+                    int subPackets = Integer.parseInt(bits.substring(bitCount, bitCount + 11), 2);
+                    this.bitCount += 11;
+                    // Parse them into packets
+                    int offset = this.bitCount;
+                    while (subPackets > 0) {
+                        Packet subPacket = new Packet(bits.substring(offset));
+                        this.subPackets.add(subPacket);
+                        subPackets--;
+                        offset += subPacket.bitCount;
+                        this.bitCount += subPacket.bitCount;
+                    }
+
+                }
+            }
+
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Packet [version=%s, type=%s, value=%s, subPackets=%s]", version, type, value, subPackets);
+        }
+
+    }
+
+}
