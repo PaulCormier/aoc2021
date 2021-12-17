1a1f6679332756f86f0c91d951cceb219b1b8486
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-17 16:25:48 -0500

    Implement Day 17 - part 2

--- src/main/java/aoc2021/Day17.java
+++ src/main/java/aoc2021/Day17.java
@@ -1,5 +1,7 @@
 package aoc2021;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.slf4j.LoggerFactory;
@@ -38,11 +40,11 @@ public class Day17 {
 
         log.setLevel(Level.DEBUG);
 
-        log.info("{}", part2(TEST_TARGET_RANGES));
+        log.info("The total number of initial velocities which end in the target area in the test data is: {}", part2(TEST_TARGET_RANGES));
 
         log.setLevel(Level.INFO);
 
-        log.info("{}", part2(TARGET_RANGES));
+        log.info("The total number of initial velocities which end in the target area in the real data is: {}", part2(TARGET_RANGES));
     }
 
     /**
@@ -94,9 +96,64 @@ public class Day17 {
         return maxHeight;
     }
 
+    /**
+     * How many distinct initial velocity values cause the probe to be within
+     * the target area after any step?
+     * 
+     * @param targetRanges
+     *            The x and y coordinate ranges in the order: minX, maxX, minY,
+     *            maxY.
+     * @return The total number of initial velocities which end in the target
+     *         area.
+     */
     private static int part2(final int[] targetRanges) {
+        int minX = targetRanges[0];
+        int maxX = targetRanges[1];
+        int minY = targetRanges[2];
+        int maxY = targetRanges[3];
 
-        return -1;
+        // We know how to hit the bottom left corner;
+        // Any higher yVelocity is going to pass straight through,
+        int yVelocityMax = -minY - 1;
+        // Any slower xVelocity is going to not make it...
+        int xVelocityMin = (int) (Math.sqrt(1 + 8 * minX) / 2);
+
+        // Maximum xVelocity to not miss the far edge of the box
+        int xVelocityMax = maxX;
+
+        // Minimum yVelocity to not miss the bottom of the box
+        int yVelocityMin = minY;
+
+        log.debug("X velocity between: {} and {}, y velocity between: {} and {}.", xVelocityMin, xVelocityMax, yVelocityMin, yVelocityMax);
+
+        List<int[]> initialVelocities = new ArrayList<>();
+
+        // Try all of the combinations?
+        for (int xVelocity = xVelocityMin; xVelocity <= xVelocityMax; xVelocity++) {
+            for (int yVelocity = yVelocityMin; yVelocity <= yVelocityMax; yVelocity++) {
+                // Determine how many steps to try?
+
+                Probe probe = new Probe(xVelocity, yVelocity);
+
+                // If the probe is out of bounds, stop
+                while (probe.xPosition <= maxX && probe.yPosition >= minY) {
+                    if (probe.xPosition >= minX && probe.yPosition <= maxY) {
+                        initialVelocities.add(new int[] { xVelocity, yVelocity });
+                        break;
+                    }
+                    probe.step();
+                }
+            }
+        }
+
+        StringBuilder results = new StringBuilder();
+        int i = 0;
+        for (int[] initialVelocity : initialVelocities) {
+            results.append(initialVelocity[0]).append(",").append(initialVelocity[1]).append(++i % 9 == 0 ? "\n" : "\t");
+        }
+        log.debug("Initial velocity values that meet these criteria:\n{}", results.toString());
+
+        return initialVelocities.size();
     }
 
     private static class Probe {
