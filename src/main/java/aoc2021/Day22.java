bbd73b3b3ce80eb7616300e1ffaa22c551fb1a93
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-22 11:43:02 -0500

    Checkpoint Day 22 - part 2
    The volume calculation isn't quite working... I need to have a closer
    look later.

--- src/main/java/aoc2021/Day22.java
+++ src/main/java/aoc2021/Day22.java
@@ -1,12 +1,15 @@
 package aoc2021;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
@@ -37,27 +40,28 @@ public class Day22 {
                                                         .split("\n\n"));
         log.trace(testLines.toString());
 
-        log.info("{} cubes are on in the test data.", part1(Arrays.asList(testLines.get(0).split("\n"))));
+        // log.info("{} cubes are on in the test data.",
+        // part1(Arrays.asList(testLines.get(0).split("\n"))));
 
         log.setLevel(Level.INFO);
 
         // Read the real file
         List<String> lines = FileUtils.readFile(INPUT_TXT);
 
-        log.info("{} cubes are on in the real data.", part1(lines));
+        // log.info("{} cubes are on in the real data.", part1(lines));
 
         // PART 2
 
         log.setLevel(Level.DEBUG);
 
-        log.info("{} cubes are on in initialization region of the test data.",
-                 part1(Arrays.asList(testLines.get(1).split("\n"))));
+        // log.info("{} cubes are on in initialization region of the test data.",
+        // part1(Arrays.asList(testLines.get(1).split("\n"))));
 
         log.info("{} cubes are on in the test data.", part2(Arrays.asList(testLines.get(1).split("\n"))));
 
         log.setLevel(Level.INFO);
 
-        log.info("{} cubes are on in the real data.", part2(lines));
+        // log.info("{} cubes are on in the real data.", part2(lines));
     }
 
     /**
@@ -118,9 +122,157 @@ public class Day22 {
         return activeCubes.size();
     }
 
-    private static int part2(final List<String> lines) {
+    /**
+     * Given the larger set of data, determine how many cubes are on.
+     * 
+     * @param lines The reboot instructions.
+     * @return The number of cubes which are on.
+     */
+    private static long part2(final List<String> lines) {
 
-        return -1;
+        AtomicInteger id = new AtomicInteger(1);
+
+        List<Cuboid> cuboids = lines.stream()
+                                    .map(l -> new Cuboid(Integer.toString(id.getAndIncrement()), l))
+                                    .collect(Collectors.toList());
+
+        log.trace("\n{}", cuboids.stream().map(Cuboid::toString).collect(Collectors.joining("\n")));
+
+        // Apply the first 10 (this will give a good checkpoint for the test data)
+        List<Cuboid> processed = new ArrayList<>();
+        processed.add(cuboids.get(0));
+        for (Cuboid otherCuboid : cuboids.subList(1, 10)) {
+            for (Cuboid cuboid : processed) {
+                if (cuboid.equals(otherCuboid))
+                    continue;
+                if (cuboid.overlaps(otherCuboid)) {
+                   long difference= cuboid.subtract(otherCuboid);
+                    log.debug("{} overlaps with {} by {}.", otherCuboid, cuboid, difference);
+                }
+            }
+            processed.add(otherCuboid);
+        }
+
+        // Apply the overlaps from each subsequent cuboid
+
+        return processed.stream().filter(c -> c.on).mapToLong(Cuboid::getVolume).sum();
+    }
+
+    private static boolean isInRange(int value, int minValue, int maxValue) {
+        return minValue <= value && value <= maxValue;
+    }
+
+    /**
+     * A cubic region in 3 dimensional space.
+     */
+    private static class Cuboid {
+        String id;
+
+        boolean on;
+        int xMin;
+        int yMin;
+        int zMin;
+        int xMax;
+        int yMax;
+        int zMax;
+
+        long initialVolume;
+
+        Set<Cuboid> containedCuboids = new HashSet<>();
+
+        Cuboid(String id, String parameters) {
+            this.id = id;
+
+            this.on = parameters.startsWith("on");
+
+            // Min/max x,y,z
+            int[] coordinates = Arrays.stream(parameters.substring(on ? 3 : 4).split("(,?[xyz]=)|(\\.\\.)"))
+                                      .filter(StringUtils::isNotBlank)
+                                      .mapToInt(Integer::parseInt)
+                                      .toArray();
+            this.xMin = coordinates[0];
+            this.xMax = coordinates[1];
+            this.yMin = coordinates[2];
+            this.yMax = coordinates[3];
+            this.zMin = coordinates[4];
+            this.zMax = coordinates[5];
+
+            this.initialVolume = (xMax - xMin) * (yMax - yMin) * (zMax - zMin);
+
+        }
+
+        Cuboid(String id, boolean on, int xMin, int yMin, int zMin, int xMax, int yMax, int zMax) {
+            this.id = id;
+            
+            this.on = on;
+            this.xMin = xMin;
+            this.xMax = xMax;
+            this.yMin = yMin;
+            this.yMax = yMax;
+            this.zMin = zMin;
+            this.zMax = zMax;
+
+            this.initialVolume = (xMax - xMin) * (yMax - yMin) * (zMax - zMin);
+        }
+
+        /**
+         * Determine the volume of "on" spaces of this cuboid.
+         * 
+         * @return The number of effectively "on" cubes in this region.
+         */
+        long getVolume() {
+            // if (on)
+            return initialVolume - containedCuboids.stream().mapToLong(Cuboid::getVolume).sum();
+            // else
+            // return containedCuboids.stream().mapToLong(Cuboid::getVolume).sum();
+        }
+
+        /**
+         * Determine if this cuboid overlaps another's region.
+         * 
+         * @return true if these two cuboids share a region of space.
+         */
+        boolean overlaps(Cuboid otherCuboid) {
+            return ((isInRange(otherCuboid.xMin, this.xMin, this.xMax)
+                     || isInRange(otherCuboid.xMax, this.xMin, this.xMax))
+                    && (isInRange(otherCuboid.yMin, this.yMin, this.yMax)
+                        || isInRange(otherCuboid.yMax, this.yMin, this.yMax))
+                    && (isInRange(otherCuboid.zMin, this.zMin, this.zMax)
+                        || isInRange(otherCuboid.zMax, this.zMin, this.zMax)));
+        }
+
+        /**
+         * Create an enclosed cuboid representing the overlapping region.
+         * 
+         * @param otherCuboid The other cuboid to overlap.
+         */
+        long subtract(Cuboid otherCuboid) {
+
+            // New region boundaries
+            int xMin = Math.max(this.xMin, otherCuboid.xMin);
+            int yMin = Math.max(this.yMin, otherCuboid.yMin);
+            int zMin = Math.max(this.zMin, otherCuboid.zMin);
+            int xMax = Math.min(this.xMax, otherCuboid.xMax);
+            int yMax = Math.min(this.yMax, otherCuboid.yMax);
+            int zMax = Math.min(this.zMax, otherCuboid.zMax);
+
+            Cuboid overlap = new Cuboid(this.id + "-" + (containedCuboids.size() + 1),
+                                        otherCuboid.on, xMin, yMin, zMin, xMax, yMax, zMax);
+            // Add its overlap to any of the contained cuboids
+            for (Cuboid containedCuboid : this.containedCuboids) {
+                if (containedCuboid.overlaps(otherCuboid))
+                    containedCuboid.subtract(otherCuboid);
+            }
+            this.containedCuboids.add(overlap);
+
+            return overlap.getVolume();
+        }
+
+        @Override
+        public String toString() {
+            return String.format("#%s: %s x=%d..%d,y=%d..%d,z=%d..%d (%d)", id,
+                                 on ? "on" : "off", xMin, xMax, yMin, yMax, zMin, zMax, getVolume());
+        }
     }
 
     /**
