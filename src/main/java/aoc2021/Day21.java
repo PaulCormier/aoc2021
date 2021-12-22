d62b96e60d02fdbcb570d37d2a686ae030359eed
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2021-12-22 09:10:38 -0500

    Checkpoint Day 21 - part 2
    Working out the score progression ok, just having trouble with the wins.

--- src/main/java/aoc2021/Day21.java
+++ src/main/java/aoc2021/Day21.java
@@ -1,9 +1,12 @@
 package aoc2021;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import org.slf4j.LoggerFactory;
 
@@ -46,7 +49,7 @@ public class Day21 {
 
         log.setLevel(Level.DEBUG);
 
-        // log.info("{}", part2(TEST_P1_START, TEST_P2_START));
+        log.info("{}", part2(TEST_P1_START, TEST_P2_START));
 
         log.setLevel(Level.INFO);
 
@@ -87,9 +90,137 @@ public class Day21 {
         return (p1Score > p2Score ? p2Score : p1Score) * (die.get() - 1);
     }
 
-    private static int part2(final List<String> lines) {
+    private static int part2(int p1Start, int p2Start) {
+
+        int[] diracDie = { 0, 0, 0, 1, 3, 6, 7, 6, 3, 1 };
+        int positions = 11;
+
+        // Number of player Ns in a position
+        // (with a dummy 0 position because I don't want to deal with the index offset)
+        // long[] p1Positions = new long[positions];
+        // p1Positions[p1Start] = 1;
+        // long[] p2Positions = new long[positions];
+        // p2Positions[p2Start] = 1;
+
+        // Number of player Ns on a given space with a given score
+        long[][] p1Scores = new long[positions][30];
+        p1Scores[p1Start][0] = 1;
+        long[][] p2Scores = new long[positions][30];
+        p2Scores[p2Start][0] = 1;
+
+        int maxScore = 21;
+        int maxTurn = 10;
+
+        long games = 1;
+
+        int turn = 1;
+        long[] p1Wins = new long[maxTurn + 1];
+        long[] p2Wins = new long[maxTurn + 1];
+        while (turn <= maxTurn) {
+            log.debug("There are {} universes on turn {}.", games, turn);
+            // Player 1's turn
+            // long[] newPositions = new long[positions];
+            long[][] newScores = new long[positions][30];
+            for (int position = 1; position < positions; position++) {
+                for (int roll = 3; roll < diracDie.length; roll++) {
+                    // Move the pieces
+                    int newPosition = (position + roll - 1) % 10 + 1;
+                    // newPositions[newPosition] += p1Positions[position] * diracDie[roll];
+
+                    // Tally the scores
+                    for (int s = 0; s + newPosition < 30; s++) {
+                        newScores[newPosition][s + newPosition] += p1Scores[position][s] * diracDie[roll];
+                    }
+                }
+            }
+            log.debug("Player 1 scores:\n{}",printMap(newScores));
+            // Count winners
+            for (int position = 1; position < positions; position++) {
+                for (int score = 21; score < newScores[position].length; score++) {
+                    long winners = newScores[position][score];
+                    p1Wins[turn] += winners;
+
+                    // Remove them
+                    // newPositions[position] -= winners;
+                    newScores[position][score] = 0;
+                }
+            }
+            games *= 27;
+            // Multiply the player 2 counts by the three dice rolls.
+            for (int position = 1; position < p2Scores.length; position++) {
+                for (int score = 0; score < p2Scores[position].length; score++) {
+                    p2Scores[position][score] *= 27;
+                }
+            }
+
+            // p1Positions = newPositions;
+            p1Scores = newScores;
+
+            // Player 2's turn
+            // newPositions = new long[positions];
+            newScores = new long[positions][30];
+            for (int position = 1; position < positions; position++) {
+                for (int roll = 3; roll < diracDie.length; roll++) {
+                    // Move the pieces
+                    int newPosition = (position + roll - 1) % 10 + 1;
+                    // newPositions[newPosition] += p2Positions[position] * diracDie[roll];
+
+                    // Tally the scores
+                    for (int s = 0; s + newPosition < 30; s++) {
+                        newScores[newPosition][s + newPosition] += p2Scores[position][s] * diracDie[roll];
+                    }
+                }
+            }
+            games *= 27;
+            log.debug("Player 2 scores:\n{}",printMap(newScores));
+            // Count winners
+            for (int position = 1; position < newScores.length; position++) {
+                for (int score = 21; score < newScores[position].length; score++) {
+                    long winners = newScores[position][score];
+                    // But player 1 had to not win first
+                    double player1NoWin = (games - p1Wins[turn] * 27);
+                    p2Wins[turn] += winners / (double) games * player1NoWin;
+
+                    // Remove them
+                    // newPositions[position] -= winners;
+                    newScores[position][score] = 0;
+                }
+            }
+            // Multiply the player 1 counts by the three dice rolls.
+            for (int position = 1; position < p1Scores.length; position++) {
+                for (int score = 0; score < p1Scores[position].length; score++) {
+                    p1Scores[position][score] *= 27;
+                }
+            }
+            games -= p1Wins[turn] * 27 + p2Wins[turn];
+
+            // p2Positions = newPositions;
+            p2Scores = newScores;
+
+            log.debug("Player 1 won {} times, player 2 won {} times.", p1Wins[turn], p2Wins[turn]);
+
+            
+            turn++;
+        }
+
+        long p1TotalWins = Arrays.stream(p1Wins).sum();
+        long p2TotalWins = Arrays.stream(p2Wins).sum();
+
+        log.debug("In total player 1 won {} times, and player 2 won {}.", p1TotalWins, p2TotalWins);
 
         return -1;
+    }
+
+    private static String printMap(long[][] map) {
+        return printMap(map, ",");
+    }
+
+    private static String printMap(long[][] map, String separator) {
+        return Arrays.stream(map)
+                     .map(l -> LongStream.of(l)
+                                        .mapToObj(Long::toString)
+                                        .collect(Collectors.joining(separator)))
+                     .collect(Collectors.joining("\n"));
     }
 
 }
